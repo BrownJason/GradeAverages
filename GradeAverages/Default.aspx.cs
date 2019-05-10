05c063f5e19fd94ce0615a959c7c5c7b4bd8172e
Jason Brown <jbjbro6@gmail.com>
2019-05-10 00:24:35 -0500

    attempt to fix chart size #1

--- GradeAverages/Default.aspx.cs
+++ GradeAverages/Default.aspx.cs
@@ -40,8 +40,6 @@ namespace GradeAverages
 
             Chart1.Series[0].ChartType = System.Web.UI.DataVisualization.Charting.SeriesChartType.Column;
 
-            Chart1.ImageType = System.Web.UI.DataVisualization.Charting.ChartImageType.Jpeg;
-
             Chart1.ChartAreas.Add(new ChartArea());
             Chart1.ChartAreas[0].Area3DStyle.Enable3D = true;
 
