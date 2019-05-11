128b53a99f3a4a939cec5da9613b60c91c0445e5
Jason Brown <jbjbro6@gmail.com>
2019-05-11 03:49:08 -0500

    Added reset of chart 2 points and change from bar to Spline, might have to change back

--- GradeAverages/Default.aspx.cs
+++ GradeAverages/Default.aspx.cs
@@ -121,46 +121,113 @@ namespace GradeAverages
             Chart1.Height = 450;
 
             IEnumerable<string> healthOneG1 = (from people in peopleArray where people.Health == 1 select people.G1);
+            IEnumerable<string> healthTwoG1 = (from people in peopleArray where people.Health == 2 select people.G1);
+            IEnumerable<string> healthThreeG1 = (from people in peopleArray where people.Health == 3 select people.G1);
+            IEnumerable<string> healthFourG1 = (from people in peopleArray where people.Health == 4 select people.G1);
+            IEnumerable<string> healthFiveG1 = (from people in peopleArray where people.Health == 5 select people.G1);
             IEnumerable<string> healthOneG2 = (from people in peopleArray where people.Health == 1 select people.G2);
+            IEnumerable<string> healthTwoG2 = (from people in peopleArray where people.Health == 2 select people.G2);
+            IEnumerable<string> healthThreeG2 = (from people in peopleArray where people.Health == 3 select people.G2);
+            IEnumerable<string> healthFourG2 = (from people in peopleArray where people.Health == 4 select people.G2);
+            IEnumerable<string> healthFiveG2 = (from people in peopleArray where people.Health == 5 select people.G2);
             double healthOneG3 = (from people in peopleArray where people.Health == 1 select people.G3).Average();
+            double healthTwoG3 = (from people in peopleArray where people.Health == 2 select people.G3).Average();
+            double healthThreeG3 = (from people in peopleArray where people.Health == 3 select people.G3).Average();
+            double healthFourG3 = (from people in peopleArray where people.Health == 4 select people.G3).Average();
+            double healthFiveG3 = (from people in peopleArray where people.Health == 5 select people.G3).Average();
 
-            double h1G1 = 0;
+            double h1G1 = 0, h2G1 = 0, h3G1 = 0, h4G1 = 0, h5G1 = 0;
             foreach(string s in healthOneG1)
             {
                 h1G1 += int.Parse(s);
             }
 
+            foreach (string s in healthTwoG1)
+            {
+                h2G1 += int.Parse(s);
+            }
+            foreach (string s in healthThreeG1)
+            {
+                h3G1 += int.Parse(s);
+            }
+            foreach (string s in healthFourG1)
+            {
+                h4G1 += int.Parse(s);
+            }
+            foreach (string s in healthFiveG1)
+            {
+                h5G1 += int.Parse(s);
+            }
+
             h1G1 = h1G1 / healthOneG1.Count();
+            h2G1 = h2G1 / healthTwoG1.Count();
+            h3G1 = h3G1 / healthThreeG1.Count();
+            h4G1 = h4G1 / healthFourG1.Count();
+            h5G1 = h5G1 / healthFiveG1.Count();
 
-
-            double h1G2 = 0;
+            double h1G2 = 0, h2G2 = 0, h3G2 = 0, h4G2 = 0, h5G2 = 0;
             foreach (string s in healthOneG2)
             {
                 h1G2 += int.Parse(s);
             }
 
+            foreach (string s in healthTwoG2)
+            {
+                h2G2 += int.Parse(s);
+            }
+            foreach (string s in healthThreeG2)
+            {
+                h3G2 += int.Parse(s);
+            }
+            foreach (string s in healthFourG2)
+            {
+                h4G2 += int.Parse(s);
+            }
+            foreach (string s in healthFiveG2)
+            {
+                h5G2 += int.Parse(s);
+            }
+
             h1G2 = h1G2 / healthOneG2.Count();
+            h2G2 = h2G2 / healthTwoG2.Count();
+            h3G2 = h3G2 / healthThreeG2.Count();
+            h4G2 = h4G2 / healthFourG2.Count();
+            h5G2 = h5G2 / healthFiveG2.Count();
+
+            double[] healthG1 = { double.Parse(String.Format("{0:0.00}", h1G1)),
+                double.Parse(String.Format("{0:0.00}", h2G1)),
+                double.Parse(String.Format("{0:0.00}", h3G1)),
+                double.Parse(String.Format("{0:0.00}", h4G1)),
+                double.Parse(String.Format("{0:0.00}", h5G1)) };
+            double[] healthG2 = { double.Parse(String.Format("{0:0.00}", h1G2)),
+                double.Parse(String.Format("{0:0.00}", h2G2)),
+                double.Parse(String.Format("{0:0.00}", h3G2)),
+                double.Parse(String.Format("{0:0.00}", h4G2)),
+                double.Parse(String.Format("{0:0.00}", h5G2)) };
+            double[] healthG3 = { healthOneG3, healthTwoG3, healthThreeG3, healthFourG3, healthFiveG3};
 
             Chart2.Series.Add("Series2");
-            Chart2.Series[0].Points.AddXY("1", double.Parse(String.Format("{0:0.00}", h1G1)));
+            Chart2.Series[0].Points.DataBindXY("1", healthG1);
             Chart2.Series[0].Font = new System.Drawing.Font("Times", 16f);
 
             Chart2.Series.Add("Series3");
-            Chart2.Series[1].Points.AddXY("1", double.Parse(String.Format("{0:0.00}", h1G2)));
+            Chart2.Series[1].Points.DataBindXY("1", healthG2);
             Chart2.Series[1].Font = new System.Drawing.Font("Times", 16f);
 
             Chart2.Series.Add("Series4");
-            Chart2.Series[2].Points.AddXY("1", double.Parse(String.Format("{0:0.00}", healthOneG3)));
+            Chart2.Series[2].Points.DataBindXY("1", healthG3);
             Chart2.Series[2].Font = new System.Drawing.Font("Times", 16f);
 
-            Chart2.Series[0].ChartType = System.Web.UI.DataVisualization.Charting.SeriesChartType.Column;
+            Chart2.Series[0].ChartType = System.Web.UI.DataVisualization.Charting.SeriesChartType.Spline;
             Chart2.Series[0].IsValueShownAsLabel = true;
+            Chart2.Series[1].IsValueShownAsLabel = true;
+            Chart2.Series[2].IsValueShownAsLabel = true;
             Chart2.Series[0].AxisLabel = "Overall Health Scale";
 
             Chart2.ChartAreas.Add("ChartArea2");
             Chart2.ChartAreas[0].Area3DStyle.Enable3D = true;
             Chart2.ChartAreas[0].AxisX.Minimum = 0;
-            Chart2.ChartAreas[0].AxisX.Title = "Travel time in minutes";
+            Chart2.ChartAreas[0].AxisX.Title = "Overall Health Scale";
             Chart2.ChartAreas[0].AxisX.Interval = 1;
 
             Chart2.Series[0].LegendText = "G1";
