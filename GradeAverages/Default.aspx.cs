5c6ae192e2d50e2fe4d38f15f2a01f543c3fe1ad
Jason Brown <jbjbro6@gmail.com>
2019-05-11 03:58:09 -0500

    Change from 3D to 2D

--- GradeAverages/Default.aspx.cs
+++ GradeAverages/Default.aspx.cs
@@ -223,10 +223,8 @@ namespace GradeAverages
             Chart2.Series[0].IsValueShownAsLabel = true;
             Chart2.Series[1].IsValueShownAsLabel = true;
             Chart2.Series[2].IsValueShownAsLabel = true;
-            Chart2.Series[0].AxisLabel = "Overall Health Scale";
 
             Chart2.ChartAreas.Add("ChartArea2");
-            Chart2.ChartAreas[0].Area3DStyle.Enable3D = true;
             Chart2.ChartAreas[0].AxisX.Minimum = 0;
             Chart2.ChartAreas[0].AxisX.Title = "Overall Health Scale";
             Chart2.ChartAreas[0].AxisX.Interval = 1;
